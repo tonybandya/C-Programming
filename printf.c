52fe436b4d619945df2f46a9b29d16cbd5ee69a7
tonybandya <levistony62@gmail.com>
2025-06-22 14:09:14 +0300

    printf

---
+++ printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	
+	
+printf("I love Melon.\n");
+printf("my favorite %s is %d\n", "number", 300);
+
+int favNum = 300;
+
+printf("my favorite %s is %d\n", "number", favNum);
+char myChar = 'i';
+printf("my favorite %c is %d\n", "myChar", favNum);
+printf("my favorite %s is %f", "number", 0.99);
+	return 0;
+}
+
