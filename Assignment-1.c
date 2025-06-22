c9b854ff9aece0c4be5b66e1ffca9b9cab9b6c5e
tonybandya <levistony62@gmail.com>
2025-06-22 03:31:23 +0300

    services to all

---
+++ Assignment-1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("-");
+	printf("\nWelcome Admin");
+	printf("\n1) WithDraw Money For Client");
+	printf("\n2) Deposit For Client");
+	printf("\n3) Register New Client");
+	printf("\n4) Check Clients Account");
+	printf("\n5) Reset Admin Login Details");
+	printf("\n6) Go back to the mainMenu");
+	printf("\n7) Exit");
+	return 0;
+}
